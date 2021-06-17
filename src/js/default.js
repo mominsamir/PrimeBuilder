0d1596c952c6379339d8df9cb4311d02a6e4f8e1
Samir Momin <mominsamir1@gmail.com>
2021-06-17 14:09:23 -0600

    first commit

---
+++ src/js/default.js
@@ -0,0 +1,16 @@
+// Default Javascript File
+
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
+
+lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true
+});
